acefc458012a3130920b7075f7304c8dfdb3d1a7
LenaGo1 <58622261+LenaGo1@users.noreply.github.com>
2021-03-15 23:29:38 +0200

    changes background-color in interval

--- index.js
+++ index.js
@@ -16,22 +16,19 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-
 const changeBgColor = () => {
     document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
   }
 const startChangeBgColor = function() {
   timerId = setInterval(changeBgColor, 1000);
-  console.log(timerId);
   refs.startButton.removeEventListener('click', startChangeBgColor);
 }
+
 refs.startButton.addEventListener('click', startChangeBgColor);
-console.log(timerId);
+
 
 const stopChangeBgColor = function () {
-  console.log(timerId);
   clearInterval(timerId);
-
   refs.startButton.addEventListener('click', startChangeBgColor);
 }
 
@@ -40,23 +37,3 @@ refs.stopButton.addEventListener('click', stopChangeBgColor);
 
 
 
-// const btn = document.querySelector("#btn");
-
-// // event - это и есть обьект события который автоматически 
-// // передается первым аргументом, мы можем называть его как угодно, 
-// // хоть qwerty, но чаще всего он называется или e, evt или event
-
-// const handleClick = event => {
-//   console.log("event: ", event); //откройте консоль чтобы увидеть весь объект целиком
-
-//   console.log("event type: ", event.type); // event type:  click
-
-//   // будет window, так как мы используем стрелочную функцию, 
-//   // которая использует внешний контекст 
-//   // Если использовать обычную функцию то this будет самим элементом 
-//   // button и равен currentTarget
-//   console.log("this: ", this); 
-//   console.log("target: ", event.target); // <button id="btn">CLICK ME!</button>
-// };
-
-// btn.addEventListener("click", handleClick);
